33ea3ae5259e5e66d2ebc3b5cebfb09f36935837
leandrofahur <fahurleandro@gmail.com>
2021-05-15 00:23:46 +0100

    updating the python2.x to python3.x

--- mac_changer.py
+++ mac_changer.py
@@ -26,21 +26,25 @@ def change_mac(interface, new_mac):
 
 def get_current_mac(interface):
     ifconfig_result = subprocess.check_output(['ifconfig', interface])
-    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
 
     if(mac_address_search_result):
         return mac_address_search_result.group(0)
     else:
         print('[-] Sorry, could not get MAC address.')
 
-
 options = get_arguments()
 current_mac = get_current_mac(options.interface)
-
 print('Current MAC = ' + str(current_mac))
-# change_mac(options.interface, options.new_mac)
 
+change_mac(options.interface, options.new_mac)
 
+current_mac = get_current_mac(options.interface)
+
+if(current_mac == options.new_mac):
+    print('[+] MAC address was successfully changed to ' + current_mac)
+else:
+    print('[-] MAC address did not get changed.')
 
 # 08:00:27:ca:45:08
 
